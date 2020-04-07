cf3c53d240c3d5b40e1949d2d6d49737f197c1ed
Swapnil Khopade <swapnilkhopade11@gmail.com>
2020-04-08 02:05:17 +0530

    Added code for Auto table creation

--- src/main/java/com/management/model/User.java
+++ src/main/java/com/management/model/User.java
@@ -1,13 +1,31 @@
 package com.management.model;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.relational.core.mapping.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
+@Table(value = "USER")
 public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id; 
+	@Basic(optional = false)
 	private String firstName;
+	@Basic(optional = false)
 	private String lastName;
+	@Basic(optional = false)
 	private String address;
-	private int age;
+	@Basic(optional = false)
+	private Integer age;
+	@Basic(optional = false)
 	private String role;
 }
